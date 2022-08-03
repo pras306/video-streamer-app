5483c0b14f946512edc63b0243e020de79aa1e5c
pras306 <44067654+pras306@users.noreply.github.com>
2022-08-03 23:28:32 +0530

    Initial Commit
    - Project's first commit

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfyqEA66G4Unh2BqBBjANj_vJLl1LZC5U",
-  authDomain: "ace-streamer-4cb89.firebaseapp.com",
-  projectId: "ace-streamer-4cb89",
-  storageBucket: "ace-streamer-4cb89.appspot.com",
-  messagingSenderId: "87212560744",
-  appId: "1:87212560744:web:9da02a30889c0f7cc206dd"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECTID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APPID}`
 };
 
 
