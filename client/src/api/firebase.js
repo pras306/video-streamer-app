5fd1d93f1fe9a56b23c2080eb91be07a8fc56521
pras306 <44067654+pras306@users.noreply.github.com>
2022-08-17 02:20:47 +0530

    MODIFY Commit
    - Modified firebase.js to implement Firestore database
    - Modified Login.js to save user data into Firestore collection
    - Modified Login.js to resolve user being able to click active tab issue

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
@@ -17,4 +18,6 @@ const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+const db = getFirestore(app);
+
+export { auth, provider, db };
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,17 +5,20 @@ import {
     signInWithEmailAndPassword,
     signInWithPopup
 } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import './Login.css';
-import { auth, provider } from '../../api/firebase';
+import { auth, provider, db } from '../../api/firebase';
 import { signIn } from '../../features/User/UserSlice';
 
 const Login = () => {
     const [ isLogin, setIsLogin ] = useState(true);
+    const [ username, setUsername ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+    const usrnameRef = useRef();
     const emailRef = useRef();
     const passwordRef = useRef();
     const dispatch = useDispatch();
@@ -23,6 +26,14 @@ const Login = () => {
 
     const validateInputs = () => {
         let isValid = true;
+
+        if((!username || username.length <= 0) && !isLogin) {
+            setUsername('');
+            usrnameRef.current.placeholder = 'Invalid Username';
+            usrnameRef.current.style.animation = "shake 1s ease forwards";
+            isValid =  false;
+        }
+
         if(!email || !(/^[a-zA-Z0-9-._%+]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(email))) {
             setEmail('');
             emailRef.current.placeholder = 'Invalid Email ID';
@@ -41,18 +52,29 @@ const Login = () => {
     };
 
     const updateStateWithUser = (userCredential) => {
-        let user = {
-            username: userCredential.user.displayName,
-            email: userCredential.user.email,
-            userId: userCredential.user.uid
-        };
-        dispatch(signIn(user));
+        // let user = {
+        //     username: userCredential.user.displayName,
+        //     email: userCredential.user.email,
+        //     userId: userCredential.user.uid
+        // };
+        dispatch(signIn(userCredential));
         navigate('/');
-    }
+    };
+
+    const handleTabSwitch = (activeTab) => {
+        if(isLogin && activeTab === 'login') return;
+        if(!isLogin && activeTab === 'register') return;
+
+        setIsLogin(!isLogin);
+    };
 
     const handleLogin = () => {
         if(!validateInputs()) {
             setTimeout(() => {
+                if(!isLogin) {
+                    usrnameRef.current.style.animation = '';
+                    usrnameRef.current.placeholder = 'Enter your username';
+                }
                 emailRef.current.style.animation = '';
                 emailRef.current.placeholder = 'Enter your email';
                 passwordRef.current.placeholder = 'Enter your password';
@@ -63,15 +85,44 @@ const Login = () => {
         if(isLogin) {
             signInWithEmailAndPassword(auth, email, password)
             .then(userCredential => {
-                updateStateWithUser(userCredential);
+                const docRef = doc(db, 'users', userCredential.user.uid);
+                getDoc(docRef)
+                .then(doc => {
+                    if(doc.exists()) {
+                        updateStateWithUser({
+                            username: doc.data().username,
+                            email: userCredential.user.email,
+                            userId: userCredential.user.uid
+                        });
+                    } else {
+                        alert('There is no user with that email.');
+                    }
+                })
+                .catch(err => {
+                    alert(err.message);
+                });
             })
             .catch(err => {
-                console.log(err);
+                alert(err.message);
             });
         } else {
             createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                updateStateWithUser(userCredential);
+                setDoc(doc(db, 'users', userCredential.user.uid), {
+                    username,
+                    email,
+                    streams: {}
+                })
+                .then(() => {
+                    updateStateWithUser({
+                        username,
+                        email,
+                        userId: userCredential.user.uid
+                    });
+                })
+                .catch(err => {
+                    alert(err.message);
+                });
             })
             .catch(err => {
                 alert(err.message);
@@ -82,7 +133,36 @@ const Login = () => {
     const handleGoogleLogin = () => {
         signInWithPopup(auth, provider)
         .then(userCredential => {
-            updateStateWithUser(userCredential);
+            const docRef = doc(db, 'users', userCredential.user.uid);
+                getDoc(docRef)
+                .then(docSnap => {
+                    if(docSnap.exists()) {
+                        updateStateWithUser({
+                            username: docSnap.data().username,
+                            email: userCredential.user.email,
+                            userId: userCredential.user.uid
+                        });
+                    } else {
+                         setDoc(doc(db, 'users', userCredential.user.uid), {
+                            username: userCredential.user.displayName,
+                            email: userCredential.user.email,
+                            streams: {}
+                        })
+                        .then(() => {
+                            updateStateWithUser({
+                                username: userCredential.user.displayName,
+                                email: userCredential.user.email,
+                                userId: userCredential.user.uid
+                            });
+                        })
+                        .catch(err => {
+                            alert(err.message);
+                        });
+                    }
+                })
+                .catch(err => {
+                    alert(err.message);
+                });
         })
         .catch(err => {
             alert(err.message);
@@ -93,13 +173,26 @@ const Login = () => {
         <div className='app__login'>
             <div className="app__login-tab">
                 <span 
-                    className={isLogin ? 'active' : ''} onClick={() => setIsLogin(!isLogin)}
+                    className={isLogin ? 'active' : ''} onClick={() => handleTabSwitch('login')}
                 >Login</span>
                 <span 
                     className={!isLogin ? 'active' : ''}
-                    onClick={() => setIsLogin(!isLogin)}
+                    onClick={() => handleTabSwitch('register')}
                 >Register</span>
             </div>
+            { !isLogin &&
+                <div className="app__login-input">
+                    <label htmlFor='username'>Username</label>
+                    <input 
+                        type={'text'} 
+                        name='username'
+                        placeholder='Enter your username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        ref={usrnameRef}
+                    />
+                </div>
+            }
             <div className="app__login-input">
                 <label htmlFor='email'>Email</label>
                 <input 
