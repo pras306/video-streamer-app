4af13f4694b8c6ea214f0c52be007518afd91855
pras306 <44067654+pras306@users.noreply.github.com>
2022-08-19 03:18:44 +0530

    MODIFY Commit - Updated component to get data from Firestore and display in UI - Passed stream data as props to StreamCard component - Updated component's styles

--- client/src/components/StreamList/StreamList.js
+++ client/src/components/StreamList/StreamList.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
 
 import './StreamList.css';
+import { db } from '../../api/firebase';
+import { StreamCard } from '../index';
 
 const StreamList = () => {
+    const [streams, setStreams] = useState([]);
+
+    useEffect(() => {
+        const getStreams = async () => {
+            let response = await getDocs(collection(db, 'users'));
+            return response;
+        };
+
+        getStreams()
+        .then(data => {
+            let userStreams = [];
+            data.forEach(doc => {
+                // if(Object.entries(doc.data().streams).length > 0) userStreams.push(doc.data().streams);
+                if(doc.data().streams.length > 0) {
+                    userStreams = [...userStreams, ...doc.data().streams];
+                }
+            });
+            setStreams(userStreams);
+        })
+        .catch(err => {
+            alert(err.message)
+        });
+    }, [setStreams]);
+
     return (
-        <div>
-            Welcome to StreamList Page
+        <div className='app__stream-list'>
+            <h2>Total Number of Streams: {streams.length}</h2>
+            <span>
+                {streams?.map((stream, idx) => {
+                    return <StreamCard key={idx} title={stream.streamName} streamer={stream.streamer} isLive={stream.isLive} />
+                })}
+            </span>
         </div>
     );
 };
